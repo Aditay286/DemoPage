451843b1574e268ff32e71f99a8e06d4bbec2d3c
Aditya <adityaq256@gmail.com>
2020-07-16 11:52:56 +0530

    this is basic update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule} from '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button';
@@ -19,7 +18,6 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule
